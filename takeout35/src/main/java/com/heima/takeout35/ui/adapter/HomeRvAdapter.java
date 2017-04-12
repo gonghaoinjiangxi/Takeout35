24a290cdc87af0f3aefb9418afe429be1ad0d699
open-android <itcastsh@126.com>
2017-04-12 15:20:45 +0800

    444444444

--- takeout35/src/main/java/com/heima/takeout35/ui/adapter/HomeRvAdapter.java
+++ takeout35/src/main/java/com/heima/takeout35/ui/adapter/HomeRvAdapter.java
@@ -2,6 +2,7 @@ package com.heima.takeout35.ui.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -20,46 +21,153 @@ import butterknife.InjectView;
 
 public class HomeRvAdapter extends RecyclerView.Adapter {
 
+    public static final int TYPE_TITLE = 0;  //头布局
+    public static final int TYPE_DIVISION = 1; //分割线，即广告
+    public static final int TYPE_SELLER = 2;  //商家，分为附近商家  其他商家
+
+    public static final int GROUP_SIZE = 10;  //分页大小10个
+    /**
+     * type必须是自然排序 0/1/2/3
+     * @param position
+     * @return
+     */
+    @Override
+    public int getItemViewType(int position) {
+        //商家数量比较多，进行分组，每组之间展示广告
+        //TODO: 头布局 （附近0-附近9) 广告 （其他0-其他9) 广告 （其他10-其他19) 广告 （其他20-其他24)
+        if(position == 0){
+            return TYPE_TITLE;
+        }else if( position >= (1 + mNearby.size()) && (position -1 - mNearby.size()) % (GROUP_SIZE + 1)== 0){
+            //TODO:附近商家个数被写死了？？？
+            //找出广告的规律，第一个广告是11
+            return TYPE_DIVISION;
+        }else{
+            return TYPE_SELLER;
+        }
+    }
+
     private Context mContext;
 
     public HomeRvAdapter(Context context) {
         mContext = context;
     }
 
-    private List<String> mDatas = new ArrayList<>();
-
-    public void setDatas(List<String> datas) {
-        mDatas = datas;
+    private List<String> mNearby = new ArrayList<>();
+    private List<String> mOther = new ArrayList<>();
+    public void setDatas(List<String> nearby, List<String> other) {
+        mNearby = nearby;
+        mOther = other;
         notifyDataSetChanged();
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //做三种不同的holder
         View itemView = View.inflate(mContext, R.layout.item_home_common, null);
-        ViewHolder viewHolder = new ViewHolder(itemView);
-        return viewHolder;
+        switch (viewType){
+            case TYPE_TITLE:
+//                itemView = View.inflate(mContext, R.layout.item_home_common, null);
+                TitleHolder titleHolder = new TitleHolder(itemView);
+                return titleHolder;
+
+            case TYPE_DIVISION:
+                DivisionHolder divisionHolder = new DivisionHolder(itemView);
+                return divisionHolder;
+
+            case TYPE_SELLER:
+                SellerHolder sellerHolder = new SellerHolder(itemView);
+                return sellerHolder;
+            default:
+                Log.e("home", "竟然还有第四种holder");
+                return null;
+        }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        //设置数据
-        ViewHolder viewHolder = (ViewHolder) holder;
-        viewHolder.setData(mDatas.get(position));
+        int viewType = getItemViewType(position);
+        switch (viewType) {
+            case TYPE_TITLE:
+                TitleHolder titleHolder = (TitleHolder) holder;
+                titleHolder.setData("我是大哥，后面小弟跟好");
+                break;
+            case TYPE_DIVISION:
+                DivisionHolder divisionHolder = (DivisionHolder) holder;
+                divisionHolder.setData("----------------我是华丽的分割线--------------------");
+                break;
+            case TYPE_SELLER:
+                SellerHolder sellerHolder = (SellerHolder) holder;
+                sellerHolder.setData("我是商家：" + position);
+                break;
+
+        }
     }
 
     @Override
     public int getItemCount() {
-        if(mDatas!=null){
-            return mDatas.size();
+        //计算条目数
+        //TODO: 头布局 （附近0-附近9) 广告 （其他0-其他9) 广告 （其他10-其他19) 广告 （其他20-其他29)
+        if(mNearby.size() == 0 && mOther.size() == 0){
+            //百度外卖破产了，程序异常数据为空，没有网络，用户旅游进入无人区
+            Log.e("home", "百度外卖破产了，程序异常数据为空，没有网络，用户旅游进入无人区");
         }
-        return 0;
+        int count = 1; //头布局展示促销信息
+        if(mNearby.size() > 0){
+            count += mNearby.size(); //附近商家
+        }else{
+            Log.e("home", "附近商家为空");
+        }
+
+        if(mOther.size() >0){
+            count +=1;  //第一个广告条
+            count += mOther.size();
+            //计算广告分割线的个数
+            //如果其他商家是25个，2个广告；如果其他商家是30个，显示2个广告；如果31个，3个广告
+            count += mOther.size() / GROUP_SIZE;
+            //如果刚好整除，反向减去一个
+            if(mOther.size() % GROUP_SIZE == 0){
+                count -= 1;
+            }
+        }else{
+            Log.e("home", "其他商家为空");
+        }
+
+        return count;
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder{
+    class TitleHolder extends RecyclerView.ViewHolder{
         @InjectView(R.id.tv)
         TextView mTv;
 
-        ViewHolder(View view) {
+        TitleHolder(View view) {
+            super(view);
+            ButterKnife.inject(this, view);
+        }
+
+        public void setData(String data) {
+            mTv.setText(data);
+        }
+    }
+
+    class DivisionHolder extends RecyclerView.ViewHolder{
+        @InjectView(R.id.tv)
+        TextView mTv;
+
+        DivisionHolder(View view) {
+            super(view);
+            ButterKnife.inject(this, view);
+        }
+
+        public void setData(String data) {
+            mTv.setText(data);
+        }
+    }
+
+    class SellerHolder extends RecyclerView.ViewHolder{
+        @InjectView(R.id.tv)
+        TextView mTv;
+
+        SellerHolder(View view) {
             super(view);
             ButterKnife.inject(this, view);
         }
--- takeout35/src/main/java/com/heima/takeout35/ui/fragment/HomeFragment.java
+++ takeout35/src/main/java/com/heima/takeout35/ui/fragment/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.heima.takeout35.ui.fragment;
 
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,12 +37,14 @@ public class HomeFragment extends Fragment {
     @InjectView(R.id.ll_title_container)
     LinearLayout mLlTitleContainer;
     private HomeRvAdapter mHomeRvAdapter;
+    private ArgbEvaluator mEvaluator;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = View.inflate(getContext(), R.layout.fragment_home, null);
         ButterKnife.inject(this, rootView);
+        mEvaluator = new ArgbEvaluator();
         initRecycleView();
         return rootView;
     }
@@ -51,19 +56,51 @@ public class HomeFragment extends Fragment {
         mRvHome.setAdapter(mHomeRvAdapter);
     }
 
+    int sumY;
+    float distance = 200.0f;  //假设的最大距离
+    int startColor = 0x553190E8;
+    int endColor = 0xFF3190E8;
+
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
         //填充模拟数据
         testData();
-        mHomeRvAdapter.setDatas(mDatas);
+        mHomeRvAdapter.setDatas(mNearby, mOther);
+        //有数据后可以监听列表滚动事件
+        mRvHome.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                Log.e("home", "dy:" + dy);
+                sumY += dy;
+
+//                sumY/distance
+                int color;
+                if(sumY < 0){
+                    color = startColor;
+                }else if(sumY > distance){
+                    color = endColor;
+                }else{
+                    //算16进制
+                    color = (int) mEvaluator.evaluate(sumY/distance, startColor, endColor);
+                }
+                mLlTitleContainer.setBackgroundColor(color);
+            }
+
+        });
     }
 
-    private List<String> mDatas = new ArrayList<>();
+    private List<String> mNearby = new ArrayList<>();
+    private List<String> mOther = new ArrayList<>();
     private void testData() {
-        for(int i=0;i<100;i++){
-            mDatas.add("我是模拟数据：" + i);
+        for(int i=0;i<7;i++){
+            mNearby.add("我是附近商家：" + i);
+        }
+        for(int i=0;i<25;i++){
+            mOther.add("我是其他商家：" + i);
         }
     }
 
